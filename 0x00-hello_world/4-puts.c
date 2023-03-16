c4212d7db042d4b42928acb9455196a6ea8a3aa1
Kellyseg <Kekeliseg@gmail.com>
2023-03-16 12:09:25 -0700

    using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /**
- * Main - Entry point
+ * main - Entry point
  *
- * Description: 'using the puts function'
  * Return: Always 0 (Success)
  */
-int main:(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
