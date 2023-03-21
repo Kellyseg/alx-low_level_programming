6a082dbe1ca576fa99d14af6ec4c08989dac184e
Kellyseg <Kekeliseg@gmail.com>
2023-03-21 10:44:52 -0700

    print lowercases

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the  alphabet
+ * description: 'print lowercase alphabets'
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+}
