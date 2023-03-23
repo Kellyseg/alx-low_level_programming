4bcfd92ef04ff53fa14c572202c1e6b97f8bc6a6
Kellyseg <Kekeliseg@gmail.com>
2023-03-23 05:58:45 -0700

    multiply 3 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: character from ASCII code
+ * @c: chaaracter from ASCII code
+ *
+ * Return: always 0(success)
+ */
+int mul(int a, int c)
+{
+	return (a * c);
+}
