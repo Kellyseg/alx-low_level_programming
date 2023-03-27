0f4da889c4a6769256736fb2a828710df63ee64f
Kellyseg <Kekeliseg@gmail.com>
2023-03-27 11:53:45 -0700

    pointers project

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function that returns lenght of a string
+ *@s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	l = strlen(s);
+	return (l);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	
