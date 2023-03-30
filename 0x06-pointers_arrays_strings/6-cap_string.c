d48d189dbf6f661da748e0797c52888e9db4b5d2
Kellyseg <Kekeliseg@gmail.com>
2023-03-30 02:26:33 -0700

    capitalize words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,6 +3,40 @@
 
 /**
  * cap_string - capitalize all words of a string
+ * @s: string
  *
+ * Return: char *
  */
-char *cap_string(char *)
+char *cap_string(char *s)
+{
+	int l;
+	int i;
+	int nc;
+	int a;
+	int lc;
+	char c[] = "	 \n,;.!?\"(){}";
+
+	l = strlen(s);
+	lc = strlen(c);
+	for (i = 0; i < l; i++)
+	{
+		nc = *(s + i);
+		if (i == 0 && (nc >= 97 && nc <= 122))
+		{
+			nc = nc - 32;
+		}
+		else if (i > 0)
+		{
+			for (a = 0; a < lc; a++)
+			{
+				if (*(s + i - 1) == c[a] && (nc >= 97 && nc <= 122))
+				{
+					nc = nc - 32;
+					break;
+				}
+			}
+		}
+		*(s + i) = nc;
+	}
+	return (s);
+}
