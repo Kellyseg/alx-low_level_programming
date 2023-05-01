3ca20b8afb60c55416d96d00d3215251dc33d336
kellyseg <kekeliseg.gmail.com>
2023-05-01 14:48:00 -0700

    sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of
+ * all data of a list
+ * @head: list head
+ *
+ * Return: sum
+ * 0 if list is empty
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
